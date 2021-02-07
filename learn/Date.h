d43bcebc8e3c8f871def8ee09a60ab432595509c
amirhossein <ahbagheri01@gmail.com>
2021-02-07 15:39:24 +0330

    inline functions added

--- learn/Date.h
+++ learn/Date.h
@@ -6,6 +6,12 @@ public:
     Date(int day, int month, int year);
     void set_date(int day,int month,int year);
     void print_date();
+    // inline functions
+    int get_day(){return day;}
+    int get_month(){ return month;}
+    int get_year(){
+        return year;
+    }
 
 private:
     int day;
