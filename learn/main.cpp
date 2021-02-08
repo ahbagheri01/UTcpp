3bd6723427a6ef19aaf7678c6d25c7c0d44cb31e
amirhossein <ahbagheri01@gmail.com>
2021-02-08 15:28:50 +0330

    ball and table

--- learn/main.cpp
+++ learn/main.cpp
@@ -1,16 +1,48 @@
 #include <iostream>
 #include <vector>
-#include "person.h"
 using namespace std;
 
 int main() {
-    person p(4,"hi",course("math",12));
-    vector<course> vecp(100,course("hi",43));
-    vector<course> ve4cp(100);
+
+}
+class Table{
+public:
+    Table(int _width,int _height);
+   int  get_width(){ return width;}
+   int get_height(){
+       return height;
+   }
+
+private:
+    int width;
+    int height;
 
 
+};
+Table::Table(int _width,int _height){
+    if(_width <= 0 || _height <=0)
+        abort();
+    width = _width;
+    height = _height;
+}
+class Ball{
+public:
+    Ball(int _x,int _y,int _vx, int _vy,Table* _table);
 
-
-
+private:
+    int x;
+    int y;
+    int vx;
+    int vy;
+    Table* table;
+};
+Ball::Ball(int _x,int _y,int _vx, int _vy,Table* _table){
+    if (_x < 0 || _x > table->get_width() || _y < 0 || _y >(* _table).get_height())
+        abort();
+    x = _x;
+    y = _y;
+    vx = _vx;
+    vy = _vy;
+    table = _table;
 
 }
